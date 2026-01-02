aba8b5fc8eda2df10e67d07528a2b88536244c68
Sergey Topilsky <s.g.topilsky@gmail.com>
2026-01-02 23:38:33 +0300

    Initial Commit

---
+++ NebulaWhisper/NebulaWhisperApp.swift
@@ -0,0 +1,17 @@
+//
+//  NebulaWhisperApp.swift
+//  NebulaWhisper
+//
+//  Created by Сергей Топильский on 02.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct NebulaWhisperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
